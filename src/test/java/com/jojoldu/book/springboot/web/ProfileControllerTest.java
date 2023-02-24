81217de6f04d264d0d11c585be262053daf1ebd9
ByHorizon <rptedpa4@naver.com>
2023-02-24 15:27:49 +0900

    0224 travis configuration

---
+++ src/test/java/com/jojoldu/book/springboot/web/ProfileControllerTest.java
@@ -0,0 +1,57 @@
+package com.jojoldu.book.springboot.web;
+
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+import org.springframework.mock.env.MockEnvironment;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.Assert.*;
+
+public class ProfileControllerTest {
+
+    @Test
+    public void real_profile이_조회된다() {
+    //given (이렇게 주어지고)
+    String expectedProfile = "real1";
+        MockEnvironment env = new MockEnvironment();
+        env.addActiveProfile(expectedProfile);
+        env.addActiveProfile("oauth");
+        env.addActiveProfile("real-db");
+
+        ProfileController controller = new ProfileController(env);
+
+    //when  (이렇게 하면)
+        String profile = controller.profile();
+    //then  (이렇게 된다)
+        assertThat(profile).isEqualTo(expectedProfile);
+    }
+
+    @Test
+    public void real_profile이_없으면_첫_번째가_조회된다 () {
+        //given (이렇게 주어지고)
+        String expectedProfile = "oauth";
+        MockEnvironment env = new MockEnvironment();
+        env.addActiveProfile(expectedProfile);
+        env.addActiveProfile("real-db");
+
+        ProfileController controller = new ProfileController(env);
+
+        //when  (이렇게 하면)
+        String profile = controller.profile();
+        //then  (이렇게 된다)
+        assertThat(profile).isEqualTo(expectedProfile);
+    }
+
+    @Test
+    public void active_profile이_없으면_default가_조회된다() {
+    //given (이렇게 주어지고)
+    String expectedProfile = "default";
+        MockEnvironment env = new MockEnvironment();
+        ProfileController controller = new ProfileController(env);
+        //when  (이렇게 하면)
+        String profile = controller.profile();
+        //then  (이렇게 된다)
+        assertThat(profile).isEqualTo(expectedProfile);
+
+    }
+}
---
+++ src/test/java/com/jojoldu/book/springboot/web/ProfileControllerTest2.java
@@ -0,0 +1,40 @@
+package com.jojoldu.book.springboot.web;
+
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class ProfileControllerTest2 {
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void profile은_인증없이_호출된다() throws Exception {
+    //given (이렇게 주어지고)
+    String expected = "default";
+        ResponseEntity<String> response = restTemplate.getForEntity("/profile", String.class);
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(response.getBody()).isEqualTo(expected);
+    //when  (이렇게 하면)
+
+    //then  (이렇게 된다)
+
+    }
+
+}
